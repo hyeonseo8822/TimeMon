8b8f53357e7cfe56a3bed005f645f1c516d8f48e
조현서 <hyeonseojo929@gmail.com>
2025-06-01 22:53:39 +0900

    달력 구현 제외외

--- src/page/App.js
+++ src/page/App.js
@@ -11,36 +11,34 @@ function App() {
       <div className="character">
         <img src="/img/lemon.svg" alt="레몬이" />
       </div>
-      
+
       <div className='studyBar'>
         {/* 오늘 공부한 시간 */}
-        <div className="studyTime">
-          <img className='studyTimeBar' src='img/studyTimeBar.svg' alt='사각형' />
+        <div className='studyTimeBar'>
           <p className='timeText'>오늘 공부한 시간</p>
           <h1 className='time'>00:00:00</h1>
         </div>
 
         {/* 타이머 / 인벤토리 페이지로 이동 버튼 */}
-        <div className="pageButtons">
-          <img className='pageBar' src='img/pageBar.svg' alt='사각형' />
+        <div className="pageBar">
           {/* 클릭할 때만 실행하기 위해 콜백함수 사용 */}
-          <div className='clock'>
-            <img src='img/Clock.svg' alt='시계' />
-            <button className='timerbtn' onClick={() => navigate('/timer')}>타이머</button>
-          </div>
-          <div className='inventory'>
-            <img src='img/Inventory.svg' alt='인벤토리' />
-            <button className='inventorybtn' onClick={() => navigate('/inventory')}>인벤토리</button>
+          <div className='pageBtns'>
+            <div className='clock'>
+              <img src='img/Clock.svg' alt='시계' />
+              <button className='timerbtn' onClick={() => navigate('/timer')}>타이머</button>
+            </div>
+            <div className='inventory'>
+              <img src='img/Inventory.svg' alt='인벤토리' />
+              <button className='inventorybtn' onClick={() => navigate('/inventory')}>인벤토리</button>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className='graph'>
-        <div className='graphBack'>
-          <p className='avgText'>일일평균</p>
-          <p className='avgTime'>0시간 0분</p>
-          <StudyBarChart />
-        </div>
+      <div className='graphBack'>
+        <p className='avgText'>일일평균</p>
+        <p className='avgTime'>0시간 0분</p>
+        <StudyBarChart />
       </div>
       <BottomBar />
     </div>
--- src/page/Profile.js
+++ src/page/Profile.js
@@ -2,9 +2,24 @@ import './css/Profile.css'
 import BottomBar from '../component/BottomBar';
 function Profile() {
   return (
-     <div className="container">
-      <div className='rectangle'></div>
-      <BottomBar/>
+    <div className="container">
+      <div className='rectangle'>
+        <img className='profile' src='img/profile.svg' />
+      </div>
+      <p className='userName'>현서</p>
+      <div className='profileEdit'>
+        <p className='profileEditText'>프로필 편집</p>
+        <img className='PencilSquare' src='img/PencilSquare.svg' />
+      </div>
+      <div className='Times'>
+        <div className='accumulTime'>
+          <p className='accumulTimeText'>누적시간</p>
+        </div>
+        <div className='avgTime2'>
+          <p className='avgTime2Text'>평균시간</p>
+        </div>
+      </div>
+      <BottomBar />
     </div>
   );
 }
